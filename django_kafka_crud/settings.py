1ab3b9ffd8321fe5cc600b4e4b8102da96a7bcfe
madhvi-n <madhvinandanwar@gmail.com>
2023-05-19 17:59:28 +0530

    commit: refactor

--- django_kafka_crud/settings.py
+++ django_kafka_crud/settings.py
@@ -65,7 +65,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'django_kafka_crud.core.middleware.RateLimiterMiddleware'
 ]
 
 ROOT_URLCONF = 'django_kafka_crud.urls'
